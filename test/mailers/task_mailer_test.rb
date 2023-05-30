04cc1d826b2766d302a8222c0704e2020e9b1b13
MelroyNoronha <melmax1208@gmail.com>
2023-05-30 21:58:24 +0530

    Moved functionality from application controller to concerns

--- test/mailers/task_mailer_test.rb
+++ test/mailers/task_mailer_test.rb
@@ -10,7 +10,7 @@ class TaskMailerTest < ActionMailer::TestCase
   def test_task_mailer_is_delivering_mails
     email = TaskMailer.pending_tasks_email(@user.id).deliver_now
     assert_not ActionMailer::Base.deliveries.empty?
-    assert_equal ["no-reply@granote.com"], email.from
+    assert_equal ["no-reply@granite.com"], email.from
     assert_equal [@user.email], email.to
     assert_equal "Pending Tasks", email.subject
   end
